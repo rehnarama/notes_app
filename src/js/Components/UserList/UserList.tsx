44ec1d9377c9386ccfc37878af55c4deb07e004a
Astrid Rehn <astrid.rehn@outlook.com>
2021-04-28 20:17:53 +0200

    Fade user list item when connecting/disconected

--- src/js/Components/UserList/UserList.tsx
+++ src/js/Components/UserList/UserList.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
 import User from "src/js/Data/Users/User";
-import classes from "./UserList.module.css"
+import classes from "./UserList.module.css";
+import { ConnectionState } from "network/";
+import classNames from "classnames";
+
+const isLoading = (state: ConnectionState): boolean => {
+  return state !== "connected";
+};
 
 const UserListItem: React.FC<{ user: User }> = props => {
   return (
-    <div className={classes.userListItem} title={`${props.user.name} - ${props.user.state}`}>
+    <div
+      className={classNames({
+        [classes.userListItem]: true,
+        [classes.userListItemLoading]: isLoading(props.user.state)
+      })}
+      title={`${props.user.name} - ${props.user.state}`}
+    >
       {props.user.name.substr(0, 1).toUpperCase()}
     </div>
   );
@@ -13,7 +25,9 @@ const UserListItem: React.FC<{ user: User }> = props => {
 const UserList: React.FC<{ users: User[]; localName: string }> = props => {
   return (
     <>
-      <UserListItem user={{ id: "", name: props.localName, state: "connected" }} />
+      <UserListItem
+        user={{ id: "", name: props.localName, state: "connected" }}
+      />
       {props.users.map(user => (
         <UserListItem key={user.id} user={user} />
       ))}
