b024e7e4029b07a3864d59f3fa357046c8b815ac
Astrid Rehn <astrid.rehn@outlook.com>
2021-06-01 21:59:09 +0200

    Render all pointers

--- src/js/Rendering/PointersRenderer.tsx
+++ src/js/Rendering/PointersRenderer.tsx
@@ -4,6 +4,7 @@ import GLApp from "../GLApp";
 import GLProgram from "../GLProgram";
 import Canvas from "../Rendering/Canvas";
 import Vector2 from "../Utils/Vector2";
+import cursorImgSrc from "url:../../images/cursor@3x.png";
 
 export type Color = [number, number, number, number];
 
@@ -12,24 +13,31 @@ precision highp float;
 
 in vec4 a_position;
 in vec4 a_color;
+in vec2 a_uv;
 
 out vec4 v_color;
+out vec2 v_uv;
 
 uniform mat3 u_vp;
 
 void main() {
   gl_Position = vec4(u_vp * vec3(a_position.xy, 1), a_position.w);
   v_color = a_color;
+  v_uv = a_uv;
 }
   `;
 const fsSource = `#version 300 es
-precision highp float;
+precision mediump float;
 
 in vec4 v_color;
+in vec2 v_uv;
+
 out vec4 output_color;
 
+uniform sampler2D u_texture;
+
 void main(void) {
-  output_color = v_color;
+  output_color = texture(u_texture, v_uv);
 }`;
 
 export default class PointersRenderer extends GLProgram {
@@ -41,17 +49,27 @@ export default class PointersRenderer extends GLProgram {
   private isDirty = false;
 
   private programInfo: twgl.ProgramInfo | null = null;
-  private attributeLocations = {
+  private attrbuteLocations = {
     position: 0,
+    uv: 0,
     color: 0
+  };
+  private uniformLocations = {
+    texture: 0
   };
 
   private program: twgl.ProgramInfo;
+
+  private textures!: Record<string, WebGLTexture>;
+
+  private cursorImg = new Image()
+  private width = 20
 
   constructor(glApp: GLApp, canvas: Canvas) {
     super(glApp);
     this.canvas = canvas;
     this.program = this.initProgram(glApp);
+    this.cursorImg.src = cursorImgSrc
   }
 
   public initProgram = (app: GLApp) => {
@@ -59,16 +77,39 @@ export default class PointersRenderer extends GLProgram {
 
     this.useProgram(this.programInfo);
 
-    this.attributeLocations.position = this.glApp.gl.getAttribLocation(
+    this.attrbuteLocations.position = this.glApp.gl.getAttribLocation(
       this.programInfo.program,
       "a_position"
     );
-    this.attributeLocations.color = this.glApp.gl.getAttribLocation(
+    this.attrbuteLocations.uv = this.glApp.gl.getAttribLocation(
+      this.programInfo.program,
+      "a_uv"
+    );
+    this.attrbuteLocations.color = this.glApp.gl.getAttribLocation(
       this.programInfo.program,
       "a_color"
     );
+    this.uniformLocations.texture = this.glApp.gl.getUniformLocation(
+      this.programInfo.program,
+      "u_texture"
+    ) as number;
 
     this.setupBuffers(app);
+
+    this.textures = twgl.createTextures(app.gl, {
+      cursor: {
+        src: cursorImgSrc
+      }
+    });
+    this.glApp.gl.activeTexture(this.glApp.gl.TEXTURE0);
+    this.glApp.gl.bindTexture(this.glApp.gl.TEXTURE_2D, this.textures.cursor);
+    this.glApp.gl.uniform1i(this.uniformLocations.texture, 0);
+
+    this.glApp.gl.blendFunc(
+      this.glApp.gl.SRC_ALPHA,
+      this.glApp.gl.ONE_MINUS_SRC_ALPHA
+    );
+    this.glApp.gl.enable(this.glApp.gl.BLEND);
 
     return this.programInfo;
   };
@@ -82,72 +123,98 @@ export default class PointersRenderer extends GLProgram {
       this.vbo = this.glApp.gl.createBuffer();
       app.gl.bindBuffer(this.glApp.gl.ARRAY_BUFFER, this.vbo);
 
-      app.gl.enableVertexAttribArray(this.attributeLocations.position);
-      app.gl.enableVertexAttribArray(this.attributeLocations.color);
+      app.gl.enableVertexAttribArray(this.attrbuteLocations.position);
+      app.gl.enableVertexAttribArray(this.attrbuteLocations.uv);
+      app.gl.enableVertexAttribArray(this.attrbuteLocations.color);
 
       app.gl.vertexAttribPointer(
-        this.attributeLocations.position,
+        this.attrbuteLocations.position,
         2,
         this.glApp.gl.FLOAT,
         false,
-        4 * 6,
+        4 * 8,
         0
       );
       app.gl.vertexAttribPointer(
-        this.attributeLocations.color,
+        this.attrbuteLocations.uv,
+        2,
+        this.glApp.gl.FLOAT,
+        false,
+        4 * 8,
+        4 * 2
+      );
+      app.gl.vertexAttribPointer(
+        this.attrbuteLocations.color,
         4,
         this.glApp.gl.FLOAT,
         false,
-        4 * 6,
-        4 * 2
+        4 * 8,
+        4 * 4
       );
     }
   }
 
+  private makeQuad = (position: Vector2) => {
+    const aspectRatio = this.cursorImg.height / this.cursorImg.width;
+    const width = this.width;
+    const height = aspectRatio * width;
+
+    return [
+      position.x,
+      position.y,
+      0,
+      0,
+      1,
+      0,
+      0,
+      1,
+      position.x + width,
+      position.y,
+      1,
+      0,
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + height,
+      0,
+      1,
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + height,
+      0,
+      1,
+      1,
+      0,
+      0,
+      1,
+      position.x + width,
+      position.y,
+      1,
+      0,
+      1,
+      0,
+      0,
+      1,
+      position.x + width,
+      position.y + height,
+      1,
+      1,
+      1,
+      0,
+      0,
+      1
+    ]
+  }
+
   public loadData = (cursors: Vector2[]) => {
-    const firstCursor = cursors[0];
-    if (firstCursor) {
-      this.data = [
-        firstCursor.x,
-        firstCursor.y,
-        1,
-        0,
-        0,
-        1,
-        firstCursor.x + 20,
-        firstCursor.y,
-        1,
-        0,
-        0,
-        1,
-        firstCursor.x,
-        firstCursor.y + 30,
-        1,
-        0,
-        0,
-        1,
-        firstCursor.x,
-        firstCursor.y + 30,
-        1,
-        0,
-        0,
-        1,
-        firstCursor.x + 20,
-        firstCursor.y,
-        1,
-        0,
-        0,
-        1,
-        firstCursor.x + 20,
-        firstCursor.y + 30,
-        1,
-        0,
-        0,
-        1
-      ];
-      this.isDirty = true;
-      this.glApp.requestFrame();
-    }
+    this.data = cursors.map(this.makeQuad).flat();
+    this.isDirty = true;
+    this.glApp.requestFrame();
   };
 
   public draw = (app: GLApp) => {
@@ -167,7 +234,7 @@ export default class PointersRenderer extends GLProgram {
       );
       this.isDirty = false;
     }
-    const trianglesToDraw = this.data.length / 6;
+    const trianglesToDraw = this.data.length / 8;
 
     twgl.setUniforms(this.programInfo, {
       u_vp: this.canvas.vp_matrix
