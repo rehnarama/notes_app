13d12bc2f15b8067e3c05a2faf09778a15c0cbaa
Astrid Rehn <astrid.rehn@outlook.com>
2021-11-27 15:47:43 +0100

    Add lower limit on circle generation resolution

--- src/js/Pen/PenUtils.ts
+++ src/js/Pen/PenUtils.ts
@@ -3,6 +3,7 @@ import { Color } from "../Lines/LineRenderer";
 
 const CIRCLE_VERTICE_PER_PIXEL = 2;
 const DEFAULT_LINE_WIDTH = 1;
+const MAX_ANGLE = 0.5;
 
 export function getPointRadius(point: Point) {
   const pressure = point.pressure;
@@ -42,7 +43,7 @@ export function generateCircleVertices(
   const radius = getPointRadius(point) * thickness;
   const circumference = radius * Math.PI;
   const nVertices = CIRCLE_VERTICE_PER_PIXEL * circumference;
-  const dTheta = (2 * Math.PI) / nVertices;
+  const dTheta = Math.min((2 * Math.PI) / nVertices, MAX_ANGLE);
 
   for (let theta = 0; theta <= 2 * Math.PI; theta += dTheta) {
     const x1 = radius * Math.cos(theta) + point.x;
